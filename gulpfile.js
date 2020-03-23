2665a64c901933771bad113aba699b8e269854cd
Passionn <yzhangpan@gmail.com>
2020-03-23 18:05:12 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp =require('gulp');
 var less =require('gulp-less');
+var postcss = require('gulp-postcss');
 
 gulp.task('css',function(){
     gulp.src('demo/index.less')
         .pipe(less())
+        .pipe(
+            postcss([
+                require('postcss-write-svg')({ /* options */ })
+            ])
+        )
         .pipe(gulp.dest('demo/'))
 });
